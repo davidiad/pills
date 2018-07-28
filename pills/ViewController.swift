638335ee0d98eb82915c8a133c807c0251c5508b
David Fierstein <studio@davidiad.com>
2018-07-27 23:47:08 -0700

    Initial Commit

---
+++ pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pills
+//
+//  Created by David Fierstein on 7/27/18.
+//  Copyright © 2018 David Fierstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
