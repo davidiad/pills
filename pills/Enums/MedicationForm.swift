d038a68a3be7c84604a4c2fb6b34b46875d38033
David Fierstein <studio@davidiad.com>
2018-07-28 22:39:29 -0700

    Create entities for core data model

---
+++ pills/Enums/MedicationForm.swift
@@ -0,0 +1,18 @@
+//
+//  MedicationForm.swift
+//  pills
+//
+//  Created by David Fierstein on 7/28/18.
+//  Copyright © 2018 David Fierstein. All rights reserved.
+//
+
+import Foundation
+
+enum Form: Int {
+    case pill = 1
+    case capsule
+    case tablet
+    case liquid
+    case inhaler
+    case topical
+}
